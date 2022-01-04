d63357ac152d314747a31768ad72e3f4e8dbfa18
Nikolaj J-K <kagenogmig@gmail.com>
2022-01-04 16:59:33 +0100

    Der er noget som virker :-)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+  Dette program bruges til at gå igennem
+  data fra Google Tidslinje. Dataen er 
+  fået gennem Google Takeout.
+
+  Det kan f.eks. bruges til at se hvor
+  lang tid du har været på arbejde eller
+  i skole. I tilfældet med arbejde kan
+  man undersøge om lønnen stemmer overens.
+
+  Dette er kun tænkte eksempler og programmet
+  udvikles udelukkende for lærings skyld.
+
+  NJK
+ */
+
+using System.Text.Json;
+
+Console.WriteLine("mapsTimelineWork");
+
+string filePath = @"/home/nikolaj/Documents/Google-Project/Takeout/Lokationshistorik/Semantic Location History/2021/2021_DECEMBER.json";
+string jsonString = File.ReadAllText(filePath);
+
+FileStream myStream = File.Open(filePath, FileMode.Open);
+
+var root = JsonSerializer.Deserialize<Root>(myStream);
+
+root.timelineObjects.RemoveAll(x => x.placeVisit == null);
+
+Console.ReadKey();
