fa6376e91d9fb60f68a7509a74dd646480923761
Nikolaj J-K <kagenogmig@gmail.com>
2022-01-04 22:41:07 +0100

    Tæller alle vagter

--- Program.cs
+++ Program.cs
@@ -18,23 +18,61 @@ using System.Text.Json;
 
 Console.WriteLine("mapsTimelineWork");
 
-string filePath = @"/home/nikolaj/Documents/Google-Project/Takeout/Lokationshistorik/Semantic Location History/2021/2021_DECEMBER.json";
+string filePath = @"C:\Users\nikol\Downloads\google-timeline-data\Lokationshistorik\Semantic Location History\2021\2021_DECEMBER.json";
 string jsonString = File.ReadAllText(filePath);
 
-FileStream myStream = File.Open(filePath, FileMode.Open);
+string basePath = @"C:\Users\nikol\Downloads\google-timeline-data\Lokationshistorik\Semantic Location History\";
+Dictionary<string, int> folders = new Dictionary<string, int>() {
+  {"2020", 12},
+  {"2021", 12},
+  {"2022", 0}
+};
+string[] months = new string[] {
+  "_JANUARY",
+  "_FEBRUARY",
+  "_MARCH",
+  "_APRIL",
+  "_MAY",
+  "_JUNE",
+  "_JULY",
+  "_AUGUST",
+  "_SEPTEMBER",
+  "_OCTOBER",
+  "_NOVEMBER",
+  "_DECEMBER",
+};
 
-var root = JsonSerializer.Deserialize<Root>(myStream);
+List<string> filePaths = new List<string>();
 
-if (root == null)
+foreach (var key in folders.Keys)
 {
-  Console.WriteLine("Der er problemer med JSON-filen :c");
-  Environment.Exit(1);
+    for (int j = 0; j < folders[key]; j++)
+    {
+        filePaths.Add(Path.Combine(basePath, key, key + months[j] + ".json"));
+    }
 }
 
-root.timelineObjects.RemoveAll(x => x.placeVisit == null);
-List<TimelineObject> burgerKingVisits = root.timelineObjects.ToList<TimelineObject>();
-burgerKingVisits.RemoveAll(x => x.placeVisit.location.name != "Burger King");
+List<TimelineObject> burgerKingVisits = new List<TimelineObject>();
 
+for (int i = 0; i < filePaths.Count; i++)
+{
+    FileStream myStream = File.Open(filePaths[i], FileMode.Open);
 
+    var root = JsonSerializer.Deserialize<Root>(myStream);
+    myStream.Close();
+
+    if (root == null)
+    {
+      Console.WriteLine("Der er problemer med JSON-filen :c");
+      Environment.Exit(1);
+    }
+
+    root.timelineObjects.RemoveAll(x => x.placeVisit == null);
+    root.timelineObjects.RemoveAll(x => x.placeVisit.location.name != "Burger King");
+    burgerKingVisits.AddRange(root.timelineObjects.ToList<TimelineObject>());
+}
+
+// Skriv vagter til tekst-fil
+// tbd
 
 Console.WriteLine("Done");
