850a664450346188328f1a991ca59ec956755170
Nikolaj J-K <kagenogmig@gmail.com>
2022-01-08 21:19:47 +0100

    Formatting

--- Classes.cs
+++ Classes.cs
@@ -1,28 +1,28 @@
 public class Location
 {
-    public int latitudeE7 { get; set; }
-    public int longitudeE7 { get; set; }
-    public string? name { get; set; }
+  public int latitudeE7 { get; set; }
+  public int longitudeE7 { get; set; }
+  public string? name { get; set; }
 }
 
 public class Duration
 {
-    public string? startTimestampMs { get; set; }
-    public string? endTimestampMs { get; set; }
+  public string? startTimestampMs { get; set; }
+  public string? endTimestampMs { get; set; }
 }
 
 public class PlaceVisit
 {
-    public Location? location { get; set; }
-    public Duration? duration { get; set; }
+  public Location? location { get; set; }
+  public Duration? duration { get; set; }
 }
 
 public class TimelineObject
 {
-    public PlaceVisit? placeVisit { get; set; }
+  public PlaceVisit? placeVisit { get; set; }
 }
 
 public class Root
 {
-    public List<TimelineObject>? timelineObjects { get; set; }
+  public List<TimelineObject>? timelineObjects { get; set; }
 }
--- TestJson.cs
+++ TestJson.cs
@@ -2,37 +2,37 @@ using System.Text.Json;
 
 public class TestJson
 {
-    static void JsonTest()
+  static void JsonTest()
+  {
+    string outPath = @"/home/nikolaj/Documents/Google-Project/Takeout/Lokationshistorik/Semantic Location History/2021/out.json";
+
+    var placeVisit1 = new PlaceVisit
     {
-        string outPath = @"/home/nikolaj/Documents/Google-Project/Takeout/Lokationshistorik/Semantic Location History/2021/out.json";
+      location = new Location { latitudeE7 = 1, longitudeE7 = 2, name = "bebo" },
+      duration = new Duration { startTimestampMs = "5", endTimestampMs = "10" }
+    };
+    var placeVisit2 = new PlaceVisit
+    {
+      location = new Location { latitudeE7 = 7, longitudeE7 = 0, name = "moob" },
+      duration = new Duration { startTimestampMs = "9", endTimestampMs = "101" }
+    };
 
-        var placeVisit1 = new PlaceVisit
-        {
-            location = new Location { latitudeE7 = 1, longitudeE7 = 2, name = "bebo" },
-            duration = new Duration { startTimestampMs = "5", endTimestampMs = "10" }
-        };
-        var placeVisit2 = new PlaceVisit
-        {
-            location = new Location { latitudeE7 = 7, longitudeE7 = 0, name = "moob" },
-            duration = new Duration { startTimestampMs = "9", endTimestampMs = "101" }
-        };
-
-        var timelineObjects = new Root
-        {
-            timelineObjects = new List<TimelineObject> {
+    var timelineObjects = new Root
+    {
+      timelineObjects = new List<TimelineObject> {
         new TimelineObject { placeVisit = placeVisit1 },
         new TimelineObject { placeVisit = placeVisit2 }
     }
-        };
+    };
 
-        JsonSerializerOptions options = new JsonSerializerOptions
-        {
-            WriteIndented = true
-        };
+    JsonSerializerOptions options = new JsonSerializerOptions
+    {
+      WriteIndented = true
+    };
 
-        Console.WriteLine(JsonSerializer.Serialize<Root>(timelineObjects, options));
-        File.WriteAllText(outPath, JsonSerializer.Serialize(timelineObjects, options));
-    }
+    Console.WriteLine(JsonSerializer.Serialize<Root>(timelineObjects, options));
+    File.WriteAllText(outPath, JsonSerializer.Serialize(timelineObjects, options));
+  }
 
 }
 
