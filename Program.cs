ebd02396c2160a8161b93cdfba7d687b8345a7c8
Nikolaj J-K <kagenogmig@gmail.com>
2022-03-10 10:39:02 +0100

    Adding skeleton to count all worked hours

--- Program.cs
+++ Program.cs
@@ -23,51 +23,84 @@ var shiftOutputFileName = "vagter.txt";
 
 Console.WriteLine("mapsTimelineWork by NJK");
 
+Dictionary<string, int> folders = new Dictionary<string, int>() {
+  {"2021", 12},
+  {"2022", 3}
+};
+string[] months = new string[] {
+  "JANUARY",
+  "FEBRUARY",
+  "MARCH",
+  "APRIL",
+  "MAY",
+  "JUNE",
+  "JULY",
+  "AUGUST",
+  "SEPTEMBER",
+  "OCTOBER",
+  "NOVEMBER",
+  "DECEMBER",
+};
+
+List<string> filePaths = new List<string>();
+
+foreach (var folder in folders)
+{
+  for (int i = 0; i < folder.Value; i++)
+  {
+    string monkey = Path.Combine("SemanticLocationHistory", folder.Key, $"{folder.Key}_{months[i]}.json");
+    filePaths.Add(monkey);
+  }
+}
+
 string filePath = @"SemanticLocationHistory/2021/2021_DECEMBER.json";
-if(!File.Exists(filePath)) {
-  Console.WriteLine($"File '{filePath}' does not exist.");
-  Environment.Exit(1);
+if (!File.Exists(filePath))
+{
+    Console.WriteLine($"File '{filePath}' does not exist.");
+    Environment.Exit(1);
 }
 
 FileStream myStream = File.Open(filePath, FileMode.Open);
 
 var root = JsonSerializer.Deserialize<Root>(myStream);
 
-if (root is null || root.timelineObjects is null) {
-  Console.WriteLine("Could not deserialize json file.");
-  Environment.Exit(1);
+if (root is null || root.timelineObjects is null)
+{
+    Console.WriteLine("Could not deserialize json file.");
+    Environment.Exit(1);
 }
 
 root.timelineObjects.RemoveAll(x => x.placeVisit == null);
-List<TimelineObject> burgerKingVisits = root.timelineObjects.ToList<TimelineObject>();
-burgerKingVisits.RemoveAll(x => x.placeVisit!.location!.name != workplace);
+List<TimelineObject> workplaceVisits = root.timelineObjects.ToList<TimelineObject>();
+workplaceVisits.RemoveAll(x => x.placeVisit!.location!.name != workplace);
 
 // write the timelineobjects that represent
 // my shift. it is written in pretty json
-JsonSerializerOptions myOptions = new JsonSerializerOptions() {
-  WriteIndented=prettyWrite
+JsonSerializerOptions myOptions = new JsonSerializerOptions()
+{
+    WriteIndented = prettyWrite
 };
 
-File.WriteAllText(outputFileName, JsonSerializer.Serialize(burgerKingVisits, myOptions));
+File.WriteAllText(outputFileName, JsonSerializer.Serialize(workplaceVisits, myOptions));
 
 // write shifts
 List<string> shifts = new List<string>();
 int sum = 0;
-for (int i = 0; i < burgerKingVisits.Count; i++)
+for (int i = 0; i < workplaceVisits.Count; i++)
 {
-  PlaceVisit visit = burgerKingVisits[i].placeVisit!;
-  string start = visit.duration!.startTimestamp!;
-  DateTime startDate = Convert.ToDateTime(start);
-  string end = visit.duration!.endTimestamp!;
-  DateTime endDate = Convert.ToDateTime(end);
+    PlaceVisit visit = workplaceVisits[i].placeVisit!;
+    string start = visit.duration!.startTimestamp!;
+    DateTime startDate = Convert.ToDateTime(start);
+    string end = visit.duration!.endTimestamp!;
+    DateTime endDate = Convert.ToDateTime(end);
 
-  TimeSpan shiftDuration = endDate.Subtract(startDate);
-  sum += shiftDuration.Hours;
+    TimeSpan shiftDuration = endDate.Subtract(startDate);
+    sum += shiftDuration.Hours;
 
-  string day = $"{startDate:dd/MM dddd}";
+    string day = $"{startDate:dd/MM dddd}";
 
-  string shiftString = $"{day}: {startDate:T} -> {endDate:T}";
-  shifts.Add(shiftString);
+    string shiftString = $"{day}: {startDate:T} -> {endDate:T}";
+    shifts.Add(shiftString);
 }
 
 File.WriteAllLines(shiftOutputFileName, shifts);
